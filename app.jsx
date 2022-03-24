757b2bab372b6fbe71cb9260419d84cd70c35e29
Khj007 <88736927+Khj007@users.noreply.github.com>
2022-03-24 10:00:29 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const index = props => {
+    return (
+        <div>
+           <h1>welcome</h1>
+           <ul>
+               <li>crete new</li>
+               <li>open</li>
+               <li>run</li>
+           </ul>
+        </div>
+    );
+};
+
+index.propTypes = {
+    
+};
+
+export default index;
